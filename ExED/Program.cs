315b0c871e7c6b7684825e601e32e62ce5f8123a
diegorafael <diegorafael_si@msn.com>
2024-01-29 01:15:10 +0000

    Comentários sobre implementações

--- ExED/Program.cs
+++ ExED/Program.cs
@@ -2,9 +2,9 @@
 {
     class Program
     {
-            static void Main()
-            {
-                var input = new List<string>{
+        static void Main()
+        {
+            var input = new List<string>{
                 "Idiossincrasia",
                 "Ambivalente",
                 "Quimérica",
@@ -17,29 +17,29 @@
                 "Inefável"
             };
 
-                var result = FiltrarStringsPorTamanho(input, 10);
+            var result = FiltrarStringsPorTamanho(input, 10);
 
-                Console.WriteLine("Strings com 10 ou mais caracteres:");
-                foreach (var str in result)
-                {
-                    Console.WriteLine(str);
-                }
-            }
-
-            static List<string> FiltrarStringsPorTamanho(List<string> input, int tamanhoMinimo)
+            Console.WriteLine("Strings com 10 ou mais caracteres:");
+            foreach (var str in result)
             {
-                List<string> resultado = new List<string>();
-
-                foreach (var str in input)
-                {
-                    if (str.Length >= tamanhoMinimo)
-                    {
-                        resultado.Add(str);
-                    }
-                }
-
-                return resultado;
+                Console.WriteLine(str);
             }
+        }
+
+        static List<string> FiltrarStringsPorTamanho(List<string> input, int tamanhoMinimo)
+        {
+            List<string> resultado = new List<string>();
+
+            foreach (var str in input)
+            {
+                if (str.Length >= tamanhoMinimo)
+                {
+                    resultado.Add(str);
+                }
+            }
+
+            return resultado;
+        }
     }
 }
 
--- Pilhas/Program.cs
+++ Pilhas/Program.cs
@@ -1,16 +1,13 @@
 ﻿namespace Pilhas
 {
-    using System;
-    using System.Collections.Generic;
-
     class Program
     {
         static void Main()
         {
-           string expressaoBalanceada = "(2+1)*80/(7-[√9 + {4² * 0}])";
-           string expressaoNaoBalanceada = "([{35 - 2} + 5*3} + 0 / 15) - [3 + 5³] * 11)";
-           ImprimeMensagem(VerificarBalanceamento(expressaoBalanceada));
-           ImprimeMensagem(VerificarBalanceamento(expressaoNaoBalanceada));
+            string expressaoBalanceada = "(2+1)*80/(7-[√9 + {4² * 0}])";
+            string expressaoNaoBalanceada = "([{35 - 2} + 5*3} + 0 / 15) - [3 + 5³] * 11)";
+            ImprimeMensagem(VerificarBalanceamento(expressaoBalanceada));
+            ImprimeMensagem(VerificarBalanceamento(expressaoNaoBalanceada));
         }
 
         static void ImprimeMensagem(bool isBalanceada)
@@ -39,19 +36,19 @@
                 {
                     if (pilha.Count == 0)
                     {
-                        return false; 
+                        return false;
                     }
 
                     char topoPilha = pilha.Pop();
 
                     if (!SaoParesCorrespondentes(topoPilha, caractere))
                     {
-                        return false; 
+                        return false;
                     }
                 }
             }
 
-            return pilha.Count == 0; 
+            return pilha.Count == 0;
         }
 
         static bool SaoParesCorrespondentes(char abertura, char fechamento)
